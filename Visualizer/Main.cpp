2eba23e65265507208b91ee41810c63e0143e4fc
Nick Gaens <mail@nickgaens.com>
2010-11-28 17:43:38 +0000

    Added Qt version check.

--- Visualizer/Main.cpp
+++ Visualizer/Main.cpp
@@ -1,7 +1,10 @@
 #include <QApplication>
+#include <QMessageBox>
 #include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
+    QT_REQUIRE_VERSION(argc, argv, "4.7.0")
+
     QApplication app(argc, argv);
 
     MainWindow mainWindow;
