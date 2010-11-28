c86e23d0981a59077939c4ea14a38e78e6fafac8
Nick Gaens <mail@nickgaens.com>
2010-11-28 17:54:04 +0000

    Added the instantiation of TimeLineVisualization and made a bridge between that ones timeSpanChanged() signal and the databases loadEvents() slot.

--- Visualizer/MainWindow.cpp
+++ Visualizer/MainWindow.cpp
@@ -1,10 +1,39 @@
 #include "MainWindow.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
-    database = new Database();
+    this->database = new Database();
+    connect(this->database, SIGNAL(eventsLoaded(QVector<Event*> *)), SLOT(onEventsLoaded(QVector<Event*> *)));
+
+    createContent();
 }
 
 void MainWindow::closeEvent(QCloseEvent *e) {
-    database->close();
-    close();
+    Q_UNUSED(e);
+
+    this->database->close();
+    this->close();
 }
+
+void MainWindow::createContent() {
+    mainLayout = new QGridLayout();
+
+    this->timeLineVis = new TimeLineVisualization(this->database);
+    connect(this->timeLineVis, SIGNAL(timeSpanChanged(int, int)), this->database, SLOT(loadEvents(int, int)));
+
+    mainLayout->addWidget(timeLineVis, 0, 0);
+
+    QWidget* centralWidget = new QWidget(this);
+    centralWidget->setLayout(mainLayout);
+    this->setCentralWidget(centralWidget);
+}
+
+void MainWindow::onEventsLoaded(QVector<Event*> *events) {
+    for (int i = 0; i < events->count(); ++i) {
+        qDebug() << events->at(i)->getTime() << " "
+                 << events->at(i)->getEventType() << " "
+                 << events->at(i)->getInputType() << " "
+                 << events->at(i)->getWidget() << " "
+                 << events->at(i)->getDetails();
+    }
+}
+
--- Visualizer/MainWindow.h
+++ Visualizer/MainWindow.h
@@ -1,19 +1,30 @@
 #ifndef MAINWINDOW_H_
 #define MAINWINDOW_H_
 
-#include <QMainWindow>
 #include "Database.h"
+#include "TimeLineVisualization.h"
+
+#include <QMainWindow>
+#include <QGridLayout>
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
     private:
-        Database* database;
+        Database *database;
+
+        TimeLineVisualization* timeLineVis;
+        QGridLayout *mainLayout;
+
         void closeEvent(QCloseEvent *e);
+        void createContent();
     
     public:
         MainWindow(QWidget *parent = 0);
         ~MainWindow() {}
+
+    public slots:
+        void onEventsLoaded(QVector<Event*> *events);
 };
 
 #endif
