b2a8fb5cbcd3767e56fbb1bb04d57546cb8f748b
Inneke Ponet <inneke@ponet.be>
2011-01-07 21:32:09 +0000

    extra: muisbewegingen en muistrajecten

--- Visualizer/HeatMapVisualization.cpp
+++ Visualizer/HeatMapVisualization.cpp
@@ -7,19 +7,24 @@ HeatMapVisualization::HeatMapVisualization(QSize resolution) : QWidget() {
     lastEvents = NULL;
     height = resolution.height();
     width = resolution.width();
-    qDebug() << "height: " << height << " width: " << width;
 
     heatMap = new int*[height];
     for (int i = 0; i < height; ++i)
         heatMap[i] = new int[width];
 
+    mouseRoute = new QVector<QPoint>();
+
     clearHeatMap();
 
-    marge = 25;
+    marge = 40;
+    mouseRouteInterval = 1;
+
     leftClick = true;
     rightClick = true;
+    mouseMoveRoute = true;
     click = true;
     doubleClick = true;
+    mouseMove = true;
 
     screenWidth = 500;
     screenHeight = 300;
@@ -32,32 +37,44 @@ HeatMapVisualization::HeatMapVisualization(QSize resolution) : QWidget() {
     heatMapLabel->setMinimumHeight(screenHeight);
     heatMapLabel->setMaximumHeight(screenHeight);*/
     heatMapLabel->setPixmap(QPixmap::fromImage(*image));
+    QPushButton *showImageButton = new QPushButton("Afbeelding in oorspronkelijke grootte");
+    connect(showImageButton, SIGNAL(clicked()), this, SLOT(showImage()));
+
     mainLayout->addWidget(heatMapLabel, 0, Qt::AlignCenter);
+    mainLayout->addWidget(showImageButton,0,Qt::AlignCenter);
 
     QHBoxLayout *checkButtonLayout = new QHBoxLayout();
     QGroupBox *box1 = new QGroupBox();
     QVBoxLayout *box1Layout = new QVBoxLayout();
     leftClickCheckBox = new QCheckBox("&Linker muisklik", this);
     rightClickCheckBox = new QCheckBox("&Rechter muisklik", this);
+    mouseMoveRouteCheckBox = new QCheckBox("&Traject muisbewegingen");
     QGroupBox *box2 = new QGroupBox();
     QVBoxLayout *box2Layout = new QVBoxLayout();
     clickCheckBox = new QCheckBox("&Eén muisklik", this);
     doubleClickCheckBox = new QCheckBox("&Dubbel muisklik", this);
+    mouseMoveCheckBox = new QCheckBox("&Muisbewegingen", this);
 
     leftClickCheckBox->setCheckState(Qt::Checked);
     rightClickCheckBox->setCheckState(Qt::Checked);
+    mouseMoveRouteCheckBox->setCheckState(Qt::Checked);
     clickCheckBox->setCheckState(Qt::Checked);
     doubleClickCheckBox->setCheckState(Qt::Checked);
+    mouseMoveCheckBox->setCheckState(Qt::Checked);
 
     connect(leftClickCheckBox, SIGNAL(stateChanged(int)), this, SLOT(updateParameters(int)));
     connect(rightClickCheckBox, SIGNAL(stateChanged(int)), this, SLOT(updateParameters(int)));
+    connect(mouseMoveRouteCheckBox, SIGNAL(stateChanged(int)), this, SLOT(updateParameters(int)));
     connect(clickCheckBox, SIGNAL(stateChanged(int)), this, SLOT(updateParameters(int)));
     connect(doubleClickCheckBox, SIGNAL(stateChanged(int)), this, SLOT(updateParameters(int)));
+    connect(mouseMoveCheckBox, SIGNAL(stateChanged(int)), this, SLOT(updateParameters(int)));
 
     box1Layout->addWidget(leftClickCheckBox);
     box1Layout->addWidget(rightClickCheckBox);
+    box1Layout->addWidget(mouseMoveRouteCheckBox);
     box2Layout->addWidget(clickCheckBox);
     box2Layout->addWidget(doubleClickCheckBox);
+    box2Layout->addWidget(mouseMoveCheckBox);
 
     box1->setLayout(box1Layout);
     box2->setLayout(box2Layout);
@@ -71,11 +88,19 @@ HeatMapVisualization::HeatMapVisualization(QSize resolution) : QWidget() {
     margeSpinBox->setValue(marge);
     connect(margeSpinBox, SIGNAL(valueChanged(int)), this, SLOT(updateMarge(int)));
 
+    QLabel *mouseRouteIntervalLabel = new QLabel("Nauwkeurigheid van muisbewegingen");
+    mouseRouteIntervalSpinBox = new QSpinBox();
+    mouseRouteIntervalSpinBox->setRange(1, 100);
+    mouseRouteIntervalSpinBox->setValue(mouseRouteInterval);
+    connect(mouseRouteIntervalSpinBox, SIGNAL(valueChanged(int)), this, SLOT(updateMouseRouteInterval(int)));
+
     QGroupBox *box3 = new QGroupBox();
     QVBoxLayout *box3Layout = new QVBoxLayout();
 
     box3Layout->addWidget(margeLabel);
     box3Layout->addWidget(margeSpinBox);
+    box3Layout->addWidget(mouseRouteIntervalLabel);
+    box3Layout->addWidget(mouseRouteIntervalSpinBox);
 
     box3->setLayout(box3Layout);
     checkButtonLayout->addWidget(box3);
@@ -97,8 +122,10 @@ void HeatMapVisualization::update(QVector<Event*> *events = NULL) {
         events = lastEvents;
     else
         return;
+    int counter = 0, mouseRouteX = 0, mouseRouteY = 0;
     for (int i = 0; i < events->count(); ++i) {
-        if ((click && events->at(i)->getEventType().compare("MouseButtonPress") == 0) || (doubleClick && events->at(i)->getEventType().compare("MouseButtonDblClick") == 0)) {
+        if ((click && events->at(i)->getEventType().compare("MouseButtonPress") == 0) || (doubleClick && events->at(i)->getEventType().compare("MouseButtonDblClick") == 0) || ((mouseMove || mouseMoveRoute) && events->at(i)->getEventType().compare("MouseMove") == 0)) {
+            // enkel muiskliks (enkel en dubbel)
             //qDebug() << "Type: " << events->at(i)->getEventType();
 
             QString details = events->at(i)->getDetails();
@@ -111,33 +138,45 @@ void HeatMapVisualization::update(QVector<Event*> *events = NULL) {
 
                 //qDebug() << details << x  << y;
                 if (x >= 0 && y >= 0 && x < height && y < width)  {
-
-                    if (args.count() == 3) {
+                    if (mouseMove && events->at(i)->getEventType().compare("MouseMove") == 0)
+                        clickOnHeatMap(x,y);
+                    if ((click || doubleClick) && args.count() == 3 && events->at(i)->getEventType().compare("MouseMove") != 0) {
+                        // controleren: links en rechts filteren indien nodig (checkBoxes)
                         if (leftClick && args.at(2).indexOf("L") != -1)// args.at(2) == ""L"	" "
                             clickOnHeatMap(x,y);
                         else if (rightClick && args.at(2).indexOf("R") != -1)
                             clickOnHeatMap(x,y);
                     }
+                    if (mouseMoveRoute && events->at(i)->getEventType().compare("MouseMove") == 0) { //muisbewegingen tekenen => gemiddeld punt berekenen per mouseRouteInterval bewegingen
+                        if (counter < mouseRouteInterval) {
+                            mouseRouteX += x;
+                            mouseRouteY += y;
+                            counter++;
+                        }
+                        if(counter == mouseRouteInterval) {
+                            mouseRoute->append(QPoint(mouseRouteY/mouseRouteInterval, mouseRouteX/mouseRouteInterval));
+                            counter = 0;
+                            mouseRouteX = 0;
+                            mouseRouteY = 0;
+                        }
+                    }
                 }
             }
         }
     }
-    //printHeatMap();
 
     renderVisualization();
 }
 
 void HeatMapVisualization::clickOnHeatMap(int x, int y) {
-
+    // waar geklikt is, een hoge score toekennen, de marge errond score alsmaar verkleinen
     for (int k = -marge; k <= marge && x + k < height; ++k)
         for (int l = -marge; l <= marge && y+l < width; ++l)
             if(x + k > 0 && y + l > 0) {
                 heatMap[x + k][y + l] += (marge + 1) - max(abs(k),abs(l));
             }
 
-
-
-    if (maxClicks < heatMap[x][y])
+    if (maxClicks < heatMap[x][y])//hoogst voorkomend aantal kliks bijhouden
         maxClicks = heatMap[x][y];
 }
 
@@ -147,18 +186,18 @@ void HeatMapVisualization::renderVisualization() {
 
     for (int i = 0; i < height; ++i) {
         for (int j = 0; j < width; ++j) {
-            if (heatMap[i][j] == 0) {
+            if (heatMap[i][j] == 0) {// er is niet geklikt
                 h = 255;
                 v = 0;
                 s = 255;
             }
-            else {
+            else {// geklikt
                 s = 255;
-                float value = float(heatMap[i][j])/float(maxClicks);
+                float value = float(heatMap[i][j])/float(maxClicks);// verhouding: is op deze plaats veel geklikt
 
                 v = value * 255.0;
 
-                if (value > 0.333){
+                if (value > 0.333){//matig tot veel geklikt => de kleuren geel tot rood hiervoor gebruiken
                     float inversDiff = 3/2, min = 1/3;
                     int color = 60;//==colorRange
                     //0 rood
@@ -168,10 +207,10 @@ void HeatMapVisualization::renderVisualization() {
                     if (heatMap[i][j] > maxClicks) {
                         h = 0;
                         v = 255;//in deze omgeving is heel veel geklikt
-                        s = 230;
+                        s = 230;// => extra benadrukken
                     }
                 }
-                else {
+                else {// weinig geklikt (marge rond klik) => blauw tinten
                     float inversDiff = 3/1, min = 0;
                     int color = 240;
                     int colorRange = 180;
@@ -187,16 +226,19 @@ void HeatMapVisualization::renderVisualization() {
             image->setPixel(j, i, color.rgb());
         }
     }
+
+    QPainter painter;
+    painter.begin(image);
+    for(int i = 0; i + 1 < mouseRoute->size(); ++i){
+        QLineF line(mouseRoute->at(i), mouseRoute->at(i+1));
+
+        painter.drawLine(line);
+    }
+    painter.end();
+
     QImage scaledImage = image->scaled(QSize(screenWidth,screenHeight),Qt::KeepAspectRatio);
     heatMapLabel->setPixmap(QPixmap::fromImage(scaledImage));
-}
 
-void HeatMapVisualization::printHeatMap() {
-    qDebug() << "heatMap:";
-    for (int i = 0; i < height; ++i)
-        for (int j = 0; j < width; ++j)
-            if (heatMap[i][j] != 0)
-                qDebug() << i << ", " << j << ": " << heatMap[i][j];
 }
 
 int HeatMapVisualization::max(int a, int b) {
@@ -209,6 +251,8 @@ void HeatMapVisualization::clearHeatMap() {
     for (int i = 0; i < height; ++i)
         for (int j = 0; j < width; ++j)
             heatMap[i][j] = 0;
+
+    mouseRoute->clear();
 }
 
 void HeatMapVisualization::updateParameters(int state) {
@@ -222,6 +266,11 @@ void HeatMapVisualization::updateParameters(int state) {
     else
         rightClick = false;
 
+    if (mouseMoveRouteCheckBox->checkState() == Qt::Checked)
+        mouseMoveRoute = true;
+    else
+        mouseMoveRoute = false;
+
     if (clickCheckBox->checkState() == Qt::Checked)
         click = true;
     else
@@ -232,6 +281,11 @@ void HeatMapVisualization::updateParameters(int state) {
     else
         doubleClick = false;
 
+    if (mouseMoveCheckBox->checkState() == Qt::Checked)
+        mouseMove = true;
+    else
+        mouseMove = false;
+
     update(NULL);
 }
 
@@ -239,4 +293,32 @@ void HeatMapVisualization::updateMarge(int marge) {
     this->marge = marge;
 
     update(NULL);
+}
+
+void HeatMapVisualization::updateMouseRouteInterval(int interval) {
+    this->mouseRouteInterval = interval;
+
+    update(NULL);
+}
+
+void HeatMapVisualization::showImage() {
+    dialog = new QDialog();
+    QVBoxLayout *layout = new QVBoxLayout();
+
+    QLabel *imageLabel = new QLabel();
+    imageLabel->setPixmap(QPixmap::fromImage(*image));
+    QPushButton *okButton = new QPushButton("Ok");
+    connect(okButton, SIGNAL(clicked()), this, SLOT(closeDialog()));
+
+    layout->addWidget(imageLabel, 0, Qt::AlignCenter);
+    layout->addWidget(okButton, 0, Qt::AlignCenter);
+
+    dialog->setLayout(layout);
+
+    dialog->show();
+}
+
+void HeatMapVisualization::closeDialog() {
+    dialog->close();
+    delete dialog;
 }
--- Visualizer/HeatMapVisualization.h
+++ Visualizer/HeatMapVisualization.h
@@ -8,6 +8,10 @@
 #include <QCheckBox>
 #include <QGroupBox>
 #include <QSpinBox>
+#include <QPainter>
+#include <QPicture>
+#include <QPushButton>
+#include <QDialog>
 
 #include "Event.h"
 
@@ -15,31 +19,33 @@ class HeatMapVisualization : public QWidget {
     Q_OBJECT
 
     private:
+        QDialog * dialog;
         QVBoxLayout *mainLayout;
         QImage* image;
         QLabel *heatMapLabel;
         QCheckBox *leftClickCheckBox;
         QCheckBox *rightClickCheckBox;
+        QCheckBox *mouseMoveRouteCheckBox;
         QCheckBox *clickCheckBox;
         QCheckBox *doubleClickCheckBox;
+        QCheckBox *mouseMoveCheckBox;
         QSpinBox *margeSpinBox;
+        QSpinBox *mouseRouteIntervalSpinBox;
 
         QVector<Event*> *lastEvents;
 
         int height, width, screenHeight, screenWidth;
         int **heatMap;
+        QVector<QPoint> *mouseRoute;
 
         int maxClicks;
-        int marge;
+        int marge, mouseRouteInterval;
 
-        bool leftClick, rightClick, click, doubleClick;
+        bool leftClick, rightClick, click, doubleClick, mouseMove, mouseMoveRoute;
 
         int max(int a, int b);
         void clickOnHeatMap(int x, int y);
         void clearHeatMap();
-
-        //testfuncties
-        void printHeatMap();
 
     public:
         HeatMapVisualization(QSize resolution);
@@ -51,6 +57,9 @@ class HeatMapVisualization : public QWidget {
     private slots:
         void updateParameters(int state);
         void updateMarge(int marge);
+        void updateMouseRouteInterval(int interval);
+        void showImage();
+        void closeDialog();
 
 };
 
